a610338d28fbf4aedd0da3859f9f974dda69b475
Edoardo La Greca <elagreca@protonmail.com>
2022-07-31 20:51:30 +0200

    simplify build.sh

--- src/representer/build.sh
+++ src/representer/build.sh
@@ -2,34 +2,41 @@
 
 # use this script to build
 
+name=repr
 cc=gcc
+
+# change '.c' with '.o'
+ctoo() {
+	name="$1"
+	echo $name | sed 's/.c$/.o/'
+}
 
 compile() {
 	infile=$1
-	outfile=`echo $file | sed "s/.c$/.o/"`
+	outfile=`ctoo $infile`
 	echo "compiling $infile -> $outfile"
 	mkdir -p obj
-	$cc -Wall -Wpedantic -ansi -o obj/$outfile $infile
+	$cc -Wall -Wpedantic -ansi $dbg_flag -o obj/$outfile $infile
 }
 
-compile_dbg() {
-	infile=$1
-	outfile=`echo $file | sed "s/.c$/.o/"`
-	echo "compiling (debug) $infile -> $outfile"
-	$cc -Wall -Wpedantic -ansi -g -o obj/$outfile $infile
-}
+obj_files=""
+
+if [ "$1" -eq "dbg" ]
+then
+	dbg_flag="-g"
+fi
 
 for file in .
 do
 	# file ends in ".c"? if so, compile it
 	if [ "$file" -eq "*.c" ]
 	then
-		if [ "$1" -eq "dbg" ]
-		then
-			compile_dbg $file
-		else
-			compile $file
-		fi
+		compile $file
+		obj_files=$obj_files" `ctoo $file`"
 	fi
-	
+
+	# remove first space
+	obj_files=`echo $obj_files | sed 's/^ //'`
+
+	$cc -o $name $obj_files
 done
