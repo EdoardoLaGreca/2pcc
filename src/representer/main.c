df8b5234ed8b49b8ce34d5d9bcf13358b35147b2
Edoardo La Greca <elagreca@protonmail.com>
2022-08-01 18:48:57 +0200

    complete file management and implement rmwhsp function

--- src/representer/main.c
+++ src/representer/main.c
@@ -8,18 +8,107 @@
 /* max number of files specifiable through CLI */
 #define FILE_N_CLI_LIMIT 1024
 
+/* length of the buffer while reading a file from disk */
+#define READ_BUFF_SIZE 4096
+
 /* remove unnecessary whitespace */
 void
 rmwhsp(char** file_content)
 {
+	/* dlt_start = delete start, dlt_end = delete end, is_ws = is whitespace? */
+	int i, file_len, dlt_start = -1, dlt_end = -1, is_ws = 0, removed_len = 0;
+
+	file_len = strlen(*file_content);
+
+	for (i = 0; i < file_len; i++) {
+		switch (*file_content[i]) {
+		case ' ':
+		case '\t':
+		case '\n':
+			dlt_start = i
+			is_ws = 1;
+			break;
+		default:
+			if (is_ws) {
+				dlt_end = i-1;
+				is_ws = 0;
+				memmove(*file_content + dlt_start, *file_content + dlt_end, file_len - dlt_end);
+				removed_len += dlt_end - dlt_start;
+			}
+			break;
+		}
+	}
+
+	/* shrink the file content array by removing unused space */
+	*file_content = realloc(file_content, file_len - removed_len);
+}
+
+/* read the entire file, allocate it in heap and return it. return NULL if an error occurred. */
+char*
+readf(FILE* f)
+{
+	char* total;
+	char buffer[READ_BUFF_SIZE];
+	int bytes_read, total_len, total_i = 0;
 	
+	/* get the file size */
+	fseek(f, 0, SEEK_END);
+	total_len = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	total = malloc(total_len + 1)
+
+	/* read bytes */
+	while ((bytes_read = fread(buffer, 1, READ_BUFF_SIZE, f)) > 0) {
+		/* copy the buffer */
+		memcpy(total + total_i, buffer, bytes_read);
+		total_i += bytes_read;
+	}
+
+	/* the loop may have ended due to an error */
+	if (ferror(f)) {
+		return NULL;
+	}
+
+	/* set string terminator */
+	total[total_len] = 0;
+
+	return total;
 }
 
 /* return the source file's IR code as a string in heap */
 char*
 compile(FILE* f)
 {
-	rmwhsp
+	/* read all the file */
+	char* file_content = readf(f);
+
+	rmwhsp(&file_content);
+
+	free(file_content);
+}
+
+/* attempt to change file name (".c" -> ".2ir") in place, name_len is the length of the name array */
+void
+chgfname(char** name, int name_len)
+{
+	int i;
+
+	/* name_len - 1 is the last character (always null)
+	   name_len - 2 is the last usable character
+	   the name array must have at least 3 null characters at the end: two for the new name (whose
+	   length differs of 2 characters) and one as string terminator */
+	if (name[name_len - 3] != 0) {
+		/* not enough space for new name, do nothing */
+		return;
+	}
+
+	/* find the dot character and do the change */
+	for (i = 0; i < name_len; i++) {
+		if (*name[i] == '.') {
+			strcpy(*name + i, ".2ir");
+		}
+	}
 }
 
 int
@@ -70,12 +159,21 @@ main(int argc, char** argv)
 	/* compile all files and then close them */
 	for (i = 0; files[i] != NULL; i++) {
 		FILE* ofile; /* output file */
-		char* ofile_name = argv[i];
-		char* ir_code = compile(files[i]);
+		char* ofile_name;
+		int ifile_len, ofile_len;
 
-		
-		if 
-		FILE* out_file = fopen(argv[i], "w");
+		/* change name */
+		ifile_len = strlen(argv[i]);
+		ofile_len = ifile_len + 2
+		ofile_name = calloc(ofile_len + 1, 1);
+		chgfname(ofile_name, ofile_len + 1);
+
+		ir_code = compile(files[i]);
+
+		FILE* out_file = fopen(ofile_name, "w");
+		fputs(ir_code, out_file);
+		fclose(out_file);
+
 		fclose(files[i]);
 	}
 }
